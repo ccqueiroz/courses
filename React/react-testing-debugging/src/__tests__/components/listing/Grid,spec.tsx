dd824c093a8a5402f19b0978508c77de347eaeb9
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2023-08-21 13:14:09 -0300

    feat: Test string with matchers

--- React/react-testing-debugging/src/__tests__/components/listing/Grid,spec.tsx
+++ React/react-testing-debugging/src/__tests__/components/listing/Grid,spec.tsx
@@ -9,3 +9,9 @@ test('Number of items = 12', () => {
 test('Number of items to be greater than or equal to 12', () => {
     expect(numItems).toBeGreaterThanOrEqual(12);
 });
+
+const dataTest = coursesJson[0].title;
+
+test('There title contains React', () => {
+    expect(dataTest).toContain('React');
+});
