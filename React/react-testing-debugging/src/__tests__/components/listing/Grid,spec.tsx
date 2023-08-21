31c5d69c8ea2cf451bca96dc70d3ec65c4ee588d
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2023-08-21 13:59:53 -0300

    feat: Organize your tests with describe()

--- React/react-testing-debugging/src/__tests__/components/listing/Grid,spec.tsx
+++ React/react-testing-debugging/src/__tests__/components/listing/Grid,spec.tsx
@@ -1,35 +1,42 @@
 import coursesJson from '../../../data/courses.json';
 
-const numItems = coursesJson.length;
 
-test('Number of items = 12', () => {
-    expect(numItems).toBe(12);
+describe('Numbers test', () => {
+    const numItems = coursesJson.length;
+    it('Number of items = 12', () => {
+        expect(numItems).toBe(12);
+    });
+
+    it('Number of items to be greater than or equal to 12', () => {
+        expect(numItems).toBeGreaterThanOrEqual(12);
+    });
 });
 
-test('Number of items to be greater than or equal to 12', () => {
-    expect(numItems).toBeGreaterThanOrEqual(12);
+describe('String test', () => {
+    const dataTest = coursesJson[0].title;
+
+    it('There title contains React', () => {
+        expect(dataTest).toContain('React');
+    });
+    
 });
 
-const dataTest = coursesJson[0].title;
+describe('Arrays and Object test', () => {
+    // Arrays 
+    //use arrayContaining
+    const dataArray = ['React Native', 'React'];
 
-test('There title contains React', () => {
-    expect(dataTest).toContain('React');
-});
+    it('The list of courses contains React and React', () => {
+        expect(['React Native', 'React', 'MeteorJS']).toEqual(expect.arrayContaining(dataArray));
+    });
 
-// Arrays 
-//use arrayContaining
-const dataArray = ['React Native', 'React'];
+    //Objects
+    it('The first couse to have a property title', () => {
+        expect(coursesJson[0]).toHaveProperty('title')
+    });
 
-test('The list of courses contains React and React', () => {
-    expect(['React Native', 'React', 'MeteorJS']).toEqual(expect.arrayContaining(dataArray));
-});
-
-//Objects
-test('The first couse to have a property title', () => {
-    expect(coursesJson[0]).toHaveProperty('title')
-});
-
-//toHaveProperty(property, value)
-test('The first couse to have a property title and value of 31,266', () => {
-    expect(coursesJson[0]).toHaveProperty('views', '31,266')
+    //toHaveProperty(property, value)
+    it('The first couse to have a property title and value of 31,266', () => {
+        expect(coursesJson[0]).toHaveProperty('views', '31,266')
+    });
 });
