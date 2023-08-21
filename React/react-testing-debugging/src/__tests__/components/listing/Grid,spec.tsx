4299911af3e64052f62c3e6d3187186d379b952c
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2023-08-21 13:22:27 -0300

    Test arrays and objects with matchers

--- React/react-testing-debugging/src/__tests__/components/listing/Grid,spec.tsx
+++ React/react-testing-debugging/src/__tests__/components/listing/Grid,spec.tsx
@@ -15,3 +15,21 @@ const dataTest = coursesJson[0].title;
 test('There title contains React', () => {
     expect(dataTest).toContain('React');
 });
+
+// Arrays 
+//use arrayContaining
+const dataArray = ['React Native', 'React'];
+
+test('The list of courses contains React and React', () => {
+    expect(['React Native', 'React', 'MeteorJS']).toEqual(expect.arrayContaining(dataArray));
+});
+
+//Objects
+test('The first couse to have a property title', () => {
+    expect(coursesJson[0]).toHaveProperty('title')
+});
+
+//toHaveProperty(property, value)
+test('The first couse to have a property title and value of 31,266', () => {
+    expect(coursesJson[0]).toHaveProperty('views', '31,266')
+});
