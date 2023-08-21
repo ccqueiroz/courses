e28c3d796d401a9b9dcaa54e5e0801fdf809ed45
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2023-08-21 13:53:44 -0300

    feat: Snapshot testing with Jest

--- React/react-testing-debugging/src/App.test.tsx
+++ React/react-testing-debugging/src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('App snapshot test', () => {
+  const components = renderer.create(<App />);
+
+  const tree = components.toJSON();
+
+  expect(tree).toMatchSnapshot();
 });
