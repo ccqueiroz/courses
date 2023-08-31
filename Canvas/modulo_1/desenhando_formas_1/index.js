2707e83c452a12c5a7e508583be0d6e211d58c0b
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2023-08-30 23:31:11 -0300

    desenhando formas

--- Canvas/modulo_1/desenhando_formas_1/index.js
+++ Canvas/modulo_1/desenhando_formas_1/index.js
@@ -57,17 +57,108 @@ const interseccaoLinhas = (posInicialX, posInicialY, posFinalX, posFinalY, lineJ
     ctx.stroke();
 }
 
+const curvas = () => {
+    const ctx = canvas.getContext('2d');
+    ctx.beginPath();
+
+    ctx.strokeStyle = 'brown';
+    ctx.lineWidth = '8';
+
+    ctx.moveTo(100, 20);
+    ctx.lineTo(200, 160);
+     //params lineTo: posX, posY, posX1ControleCurva, posY1ControleCurva
+    ctx.quadraticCurveTo(230, 200, 250, 100);
+    //params lineTo: posX, posY, posX1ControleCurva, posY1ControleCurva, posX2ControleCurva, posY2ControleCurva
+    ctx.bezierCurveTo(290, -40, 300, 200, 400, 150);
+
+    // ctx.lineTo(500, 90);
+    ctx.stroke();
+}
+
+const balao = () => {
+    const ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.strokeStyle = 'brown';
+    ctx.lineWidth = '2';
+
+    ctx.moveTo(75, 25);
+    ctx.quadraticCurveTo(25, 25, 25, 62.5);
+    ctx.quadraticCurveTo(25, 100, 50, 100);
+    ctx.quadraticCurveTo(50, 120, 30, 125);
+    ctx.quadraticCurveTo(60, 120, 65, 100);
+    ctx.quadraticCurveTo(200, 110, 200, 62.5);
+    ctx.quadraticCurveTo(200, 17, 75, 25);
+
+    ctx.stroke();
+
+}
+
+const grid = () => {
+    const ctx = canvas.getContext('2d');
+    for (let y = 0; y < 600; y += 10) {
+        ctx.beginPath();
+        ctx.moveTo(0.5 + y, 0);
+        ctx.lineTo(0.5 + y, 500);
+        ctx.strokeStyle = '#ccc';
+        ctx.stroke();
+    }
+
+    for (let x = 0; x < 500; x += 10) {
+        ctx.beginPath();
+        ctx.moveTo(0, x + 0.5);
+        ctx.lineTo(600, x + 0.5);
+        ctx.strokeStyle = '#ccc';
+        ctx.stroke();
+    }
+}
+
+const coracao = () => {
+    const ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = '2';
+
+    ctx.moveTo(75, 200);
+    ctx.lineTo(50, 175);
+    ctx.lineTo(30, 200);
+    ctx.lineTo(30, 220);
+    ctx.lineTo(75, 280);
+    ctx.lineTo(120, 220);
+    ctx.lineTo(120, 200);
+    ctx.lineTo(100, 175);
+    ctx.lineTo(75, 200);
+
+    ctx.moveTo(75, 40);
+
+    ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
+    ctx.bezierCurveTo(25, 25, 25, 60, 26, 60);
+    ctx.bezierCurveTo(30, 80, 75, 110, 75, 120);
+    ctx.bezierCurveTo(135, 60, 120, 60, 123, 60);
+    ctx.bezierCurveTo(126, 40, 120, 50, 125, 45);
+    ctx.bezierCurveTo(120, 45, 120, 25, 100, 25);
+    ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
+    ctx.strokeStyle = 'purple';
+    ctx.fillStyle = 'red';
+    ctx.fill();
+    ctx.stroke();
+}
+
 const main = () => {
     if (canvas.getContext) {
-        retangulo();
-        circulo();
-        linha(10, 400, 300, 400, 'butt');
-        linha(10, 450, 300, 450, 'square');
-        linha(10, 480, 300, 480, 'round');
+        grid();
+        // retangulo();
+        // circulo();
+        // linha(10, 400, 300, 400, 'butt');
+        // linha(10, 450, 300, 450, 'square');
+        // linha(10, 480, 300, 480, 'round');
 
-        interseccaoLinhas(20, 220, 300, 220, 'miter');
-        interseccaoLinhas(20, 280, 300, 280, 'round');
-        interseccaoLinhas(20, 340, 300, 340, 'bevel');
+        // interseccaoLinhas(20, 220, 300, 220, 'miter');
+        // interseccaoLinhas(20, 280, 300, 280, 'round');
+        // interseccaoLinhas(20, 340, 300, 340, 'bevel');
+
+        // curvas();
+        // balao();
+        coracao();
     } else {
         console.log('Navegador sem suporte ao canvas')
     }
